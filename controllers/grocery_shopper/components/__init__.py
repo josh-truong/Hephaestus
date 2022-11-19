dbaa329c31f0b067dc7114223077229635bd7be3
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-11-19 10:33:51 -0700

    Added device component

--- controllers/grocery_shopper/components/__init__.py
+++ controllers/grocery_shopper/components/__init__.py
@@ -1,6 +1,7 @@
+from .constants import RobotConst, LidarConst
+from .utils import Map, Pose
+
+from .devices import Device
 from .localization import Localization
 from .mapping import Mapping
 from .slam import EKF
-
-from .utils import Map, Pose
-from .constants import RobotConst, LidarConst
