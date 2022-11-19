4a5fd4cbffc936f7a0ea0804b9e0f65d97fd6d15
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-11-19 10:35:16 -0700

    Moved device initalized to class

--- controllers/grocery_shopper/grocery_shopper.py
+++ controllers/grocery_shopper/grocery_shopper.py
@@ -1,86 +1,108 @@
 """grocery controller."""
+import matplotlib.pyplot as plt
+import numpy as np
 
-from controller import Robot
+
 from controller import Keyboard
 
-from components import RobotConst, LidarConst
-from components import Localization, Mapping, EKF
+from components import LidarConst
+from components import Device, Localization, Mapping, EKF
 
 #Initialization
 print("=== Initializing Grocery Shopper...")
 
-# create the Robot instance.
-robot = Robot()
-
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
-
-
-
-# All motors except the wheels are controlled by position control. The wheels
-# are controlled by a velocity controller. We therefore set their position to infinite.
-target_pos = (0.0, 0.0, 0.35, 0.07, 1.02, -3.16, 1.27, 1.32, 0.0, 1.41, 'inf', 'inf',0.045,0.045)
-
-robot_parts={}
-for i, part_name in enumerate(RobotConst().PART_NAMES):
-    robot_parts[part_name]=robot.getDevice(part_name)
-    robot_parts[part_name].setPosition(float(target_pos[i]))
-    robot_parts[part_name].setVelocity(robot_parts[part_name].getMaxVelocity() / 2.0)
-
-# Enable gripper encoders (position sensors)
-left_gripper_enc=robot.getDevice("gripper_left_finger_joint_sensor")
-right_gripper_enc=robot.getDevice("gripper_right_finger_joint_sensor")
-left_gripper_enc.enable(timestep)
-right_gripper_enc.enable(timestep)
-
-# Enable Camera
-camera = robot.getDevice('camera')
-camera.enable(timestep)
-camera.recognitionEnable(timestep)
-
-# Enable GPS and compass localization
-gps = robot.getDevice("gps")
-gps.enable(timestep)
-compass = robot.getDevice("compass")
-compass.enable(timestep)
-
-# Enable LiDAR
-lidar = robot.getDevice('Hokuyo URG-04LX-UG01')
-lidar.enable(timestep)
-lidar.enablePointCloud()
-
-# Enable display
-display = robot.getDevice("display")
-
-
-
-# ------------------------------------------------------------------
-# Helper Functions
-print("=== Initializing Class Components...")
-localization = Localization(RobotConst())
-mapping      = Mapping(RobotConst(), LidarConst())
+devices      = Device()
+localization = Localization()
+mapping      = Mapping()
 slam         = EKF()
 
+# Enable devices
+robot_parts = devices.set_robot_parts()
+left_gripper_enc, right_gripper_enc = devices.enable_gripper_encoders()
+gps, compass = devices.enable_gps_compass()
+lidar = devices.enable_lidar()
+display = devices.enable_display()
+keyboard = devices.enable_keyboard()
 
-# We are using a keyboard to remote control the robot
-keyboard = robot.getKeyboard()
-keyboard.enable(timestep)
 
 
 gripper_status="closed"
 
-# Main Loop
-while robot.step(timestep) != -1:
-    # pose = localization.get_pose(gps, compass)
-    slam.update()
+
+####################################
+####################################
+def calc_input():
+    v = 1.0  # [m/s]
+    yawrate = 0.1  # [rad/s]
+    u = np.array([[v, yawrate]]).T
+    return u
+
+# RFID positions [x, y]
+RFID = np.array([
+    [10.0, -2.0],
+    [15.0, 10.0],
+    [3.0, 15.0],
+    [-5.0, 20.0]])
     
+# State Vector [x y yaw v]'
+STATE_SIZE = 3
+xEst = np.zeros((STATE_SIZE, 1))
+xTrue = np.zeros((STATE_SIZE, 1))
+PEst = np.eye(STATE_SIZE)
+
+xDR = np.zeros((STATE_SIZE, 1))  # Dead reckoning
+
+# history
+hxEst = xEst
+hxTrue = xTrue
+hxDR = xTrue
+
+show_animation = True
+####################################
+####################################
+
+
+
+# Main Loop
+while devices.robot_step() != -1:
+    pose = localization.get_pose(gps, compass)
+
+
+    u = calc_input()
+    # xTrue, z, xDR, ud = slam.observation(xTrue, xDR, u, RFID)
+    # xEst, PEst = ekf_slam(xEst, PEst, ud, z)
+    # x_state = xEst[0:STATE_SIZE]
+    # # store data history
+    # hxEst = np.hstack((hxEst, x_state))
+    # hxDR = np.hstack((hxDR, xDR))
+    # hxTrue = np.hstack((hxTrue, xTrue))
+    # if show_animation:  # pragma: no cover
+    #         plt.cla()
+    #         plt.plot(RFID[:, 0], RFID[:, 1], "*k")
+    #         plt.plot(xEst[0], xEst[1], ".r")
+    #         # plot landmark
+    #         for i in range(calc_n_LM(xEst)):
+    #             plt.plot(xEst[STATE_SIZE + i * 2],
+    #                      xEst[STATE_SIZE + i * 2 + 1], "xg")
+    #         plt.plot(hxTrue[0, :],
+    #                  hxTrue[1, :], "-b")
+    #         plt.plot(hxDR[0, :],
+    #                  hxDR[1, :], "-k")
+    #         plt.plot(hxEst[0, :],
+    #                  hxEst[1, :], "-r")
+    #         plt.axis("equal")
+    #         plt.grid(True)
+    #         plt.pause(0.001)
+
+
+
     vL, vR = mapping.manual_control(keyboard, display)
-    pose = localization.pose
+    # pose = localization.pose
 
     point_cloud = mapping.get_lidar_point_cloud(lidar, pose)
     mapping.display_point_cloud(display, point_cloud)
 
-    localization.update_odometry(vL, vR, timestep, print_pose=False)
+    localization.update_odometry(vL, vR, devices.get_timestep(), print_pose=False)
 
     robot_parts["wheel_left_joint"].setVelocity(vL)
     robot_parts["wheel_right_joint"].setVelocity(vR)
