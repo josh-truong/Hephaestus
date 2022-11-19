d76c2c365d926576822b8cfc847c11f3a2e47fa5
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-11-19 16:48:46 -0700

    Added ManagerController

--- controllers/grocery_shopper/components/__init__.py
+++ controllers/grocery_shopper/components/__init__.py
@@ -4,4 +4,7 @@ from .utils import Map, Pose
 from .devices import Device
 from .localization import Localization
 from .mapping import Mapping
-from .slam import EKF
+from .slam import SLAM
+from .robotController import RobotController
+
+from .managerController import Manager
