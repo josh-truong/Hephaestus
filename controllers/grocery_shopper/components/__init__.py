ade1ffd32ff96b4a70bf142ed6ff40c00218edb9
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-11-25 14:07:22 -0700

    Update __init__.py

--- controllers/grocery_shopper/components/__init__.py
+++ controllers/grocery_shopper/components/__init__.py
@@ -1,10 +1,9 @@
 from .constants import RobotConst, LidarConst
 from .utils import Map, Pose
 
+from .manager import Manager
 from .devices import Device
 from .localization import Localization
 from .mapping import Mapping
 from .slam import SLAM
 from .robotController import RobotController
-
-from .managerController import Manager
