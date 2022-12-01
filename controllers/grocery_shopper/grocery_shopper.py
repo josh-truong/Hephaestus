3b6f4829bfe2b65e41277654a55a924695fe2245
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-11-30 21:06:29 -0700

    rm slam

--- controllers/grocery_shopper/grocery_shopper.py
+++ controllers/grocery_shopper/grocery_shopper.py
@@ -7,7 +7,7 @@ from controller import Keyboard
 from controller import Supervisor
 
 from components import Pose, Map
-from components import Manager, Localization, Mapping, SLAM, Device, RobotController, Manipulation
+from components import Manager, Localization, Mapping, SLAM, Device, RobotController, Manipulation, Planning
 
 
 
@@ -21,80 +21,67 @@ Mapping         = Mapping(m)
 # Slam            = SLAM(m)
 Device          = Device(m)
 RobotController = RobotController(m)
+planner         = Planning()
 
 m.Localization    = Localization
 m.Mapping         = Mapping
 # m.Slam            = SLAM
 m.Device          = Device
 m.RobotController = RobotController
+m.planner         = Planning
 
 # # Initialize the Webots Supervisor.
-supervisor = Supervisor()
-timeStep = Device.robot_step()
+# supervisor = Supervisor()
+# timeStep = Device.robot_step()
 
-Maniplation = Manipulation(4, supervisor, timeStep)
-
-
+# Maniplation = Manipulation(4, supervisor, timeStep)
 
 
-# planner = Planning()
-# K = 1000 # Feel free to adjust as desired
-# map = np.load("../assets/filter_map.npy")
-# starting_point = [20,200]
-# goal = np.array([325, 325])
-# nodes = planner.rrt(starting_point, goal, K, 10, map)
-# m.RobotController.set_waypoints(nodes)
 
 
-m.RobotController.set_waypoints([
-    (  4.80,  0.00,  0.00),
-    (  4.79, -2.13,  0.00),
-    ( -3.64, -2.03,  0.00),
-    ( -3.44, -3.04,  1.57),
-    (-12.53, -1.98,  0.00),
-    (-12.10,  1.50,  3.14),
-    (  4.91,  1.43,  0.00), #wut
-    (  4.50,  5.57,  0.00),
-    (-12.91,  5.55,  0.00),
-    (-12.44, -5.44, -3.14),
-    (  5.31, -5.69, -1.56),
-    ])
+
+map = np.load("assets/filter_map.npy")
+m.Mapping.Map.map = map
+m.Mapping.display_point_cloud(None, redraw=True)
+
+nodes = planner.rrt(starting_point=[122, 180], goal_point=np.array([333, 339]), k=1000, delta_q=10, map=map)
+# planner.visualize_2D_graph(map, nodes, np.array([333, 339]))
+waypoints = np.array(planner.getWaypoints(nodes, np.array([325, 325])))
+m.RobotController.set_waypoints(waypoints)
+print(waypoints[0], waypoints[1])
+for x, y, _ in waypoints:
+    x, y = m.Mapping.get_display_coords(x, y)
+    m.Device.display.setColor(0x00FF00)
+    m.Device.display.drawPixel(x, y)
+
+
+# pt1, pt2 = (4.80, 0.00), (-13.02, -5.42)
+# pt1 = Mapping.get_display_coords(pt1[0], pt1[1])
+# pt2 = Mapping.get_display_coords(pt2[0], pt2[1])
+# print(f"Start {pt1}")
+# print(f"End: {pt2}")
+# print("")
+# print(f"Start {planner.get_world_coords(pt1[0], pt1[1])}")
+# print(f"End: {planner.get_world_coords(pt2[0], pt2[1])}")
+
+
+# m.RobotController.set_waypoints([
+#     (  4.80,  0.00,  0.00),
+#     (  4.79, -2.13,  0.00),
+#     ( -3.64, -2.03,  0.00),
+#     ( -3.44, -3.04,  1.57),
+#     (-12.53, -1.98,  0.00),
+#     (-12.10,  1.50,  3.14),
+#     (  4.91,  1.43,  0.00), #wut
+#     (  4.50,  5.57,  0.00),
+#     (-12.91,  5.55,  0.00),
+#     (-12.44, -5.44, -3.14),
+#     (  5.31, -5.69, -1.56),
+#     ])
 
 
 gripper_status="closed"
 
-
-####################################
-####################################
-def calc_input():
-    v = 1.0  # [m/s]
-    yawrate = 0.1  # [rad/s]
-    u = np.array([[v, yawrate]]).T
-    return u
-
-# RFID positions [x, y]
-RFID = np.array([
-    [10.0, -2.0],
-    [15.0, 10.0],
-    [3.0, 15.0],
-    [-5.0, 20.0]])
-    
-# State Vector [x y yaw v]'
-STATE_SIZE = 0
-xEst = np.zeros((STATE_SIZE, 1))
-xTrue = np.zeros((STATE_SIZE, 1))
-PEst = np.eye(STATE_SIZE)
-
-xDR = np.zeros((STATE_SIZE, 1))  # Dead reckoning
-
-# history
-hxEst = xEst
-hxTrue = xTrue
-hxDR = xTrue
-
-show_animation = True
-####################################
-####################################
 
 
 # Main Loop
