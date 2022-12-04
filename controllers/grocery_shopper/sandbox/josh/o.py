0e38da290d473f5043753a2b6b20311dfc64b339
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-12-04 15:30:37 -0700

    rm filles

--- controllers/grocery_shopper/sandbox/josh/o.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-map = np.load('filter_map.npy')
-path = np.load('path.npy')
-
-plt.imshow(map)
-plt.scatter(path[:,0],path[:,1], s=0.3)
-plt.show()
