d3163e6f3f4eabf784b591f70e1375c190bf7aed
Keystone47 <40375785+Keystone47@users.noreply.github.com>
2022-12-05 23:29:37 -0700

    Map flip error

--- controllers/grocery_shopper/behaviors/bt_rrt.py
+++ controllers/grocery_shopper/behaviors/bt_rrt.py
@@ -33,7 +33,7 @@ class RRT(py_trees.behaviour.Behaviour):
             y = display[1] - ((display[1]*0.5) - (y * (display[1]/world[1])))
             x, y = np.clip(x, 0, display[0]-1), np.clip(y, 0, display[1]-1)
             return [int(x), int(y)]
-        x, y = get_display_coords(robotPose.x, robotPose.y)
+        x, y = get_display_coords(robotPose.x, -1 * robotPose.y)
         randPoint = np.random.randint(0,360,2)
         
         map = convertMapToConfigSpace(self.r.env.map.map)
