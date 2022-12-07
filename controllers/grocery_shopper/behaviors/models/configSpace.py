0fd7ef30a8743ebc485e4e70bea223a418ab8835
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-12-06 22:59:50 -0700

    Update configSpace.py

--- controllers/grocery_shopper/behaviors/models/configSpace.py
+++ controllers/grocery_shopper/behaviors/models/configSpace.py
@@ -1,12 +1,34 @@
 import numpy as np
 from scipy.signal import convolve2d
-
+import matplotlib.pyplot as plt
 
 class ConfigSpace:
-    def __init__(self, kernel_size=(10,10)):
+    def __init__(self, kernel_size=(3,3)):
         self.kernel = np.ones(kernel_size)
+        self.horizontal_mask = np.array([
+            [-1, -1, -1],
+            [ 2,  2,  2],
+            [-1, -1, -1],
+        ])
+        self.vertical_mask = np.array([
+            [-1, 2, -1],
+            [-1, 2, -1],
+            [-1, 2, -1],
+        ])
 
     def run(self, map):
+        # map[map > 0.5] = 1
+        # map[map < 0.5] = 0
+        map = self.filter_by_convolving(map,0.5)
         map = convolve2d(map, self.kernel, mode='same')
-        map[map > 0] = 1
+        map[map > np.percentile(map, 25)] = 1
         return map
+
+    def filter_by_convolving(self, map, ftol):
+        grad = convolve2d(map, self.horizontal_mask, mode='same')
+        grad += convolve2d(map, self.vertical_mask, mode='same')
+        idx = grad >= ftol
+        grad[~idx] = 0
+        grad[idx] = map[idx]
+        grad[idx] = np.clip(grad[idx], 0.0, 1.0)
+        return grad
