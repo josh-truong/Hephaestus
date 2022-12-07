393d55e3897c3791df94bfe6d56b191b0c3a5ffb
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-12-06 18:24:31 -0700

    Update blackboard.py

--- controllers/grocery_shopper/behaviors/blackboard.py
+++ controllers/grocery_shopper/behaviors/blackboard.py
@@ -22,8 +22,8 @@ class Blackboard:
             "device/left_camera", "device/right_camera", "device/meta_camera", 
             "device/range_finder", "device/depth_display", "device/disable_lidar",
             "env/map", "env/refresh_hz", "env/ftol", "env/waypoints", "env/state", 
-            "env/goal", "env/object_location", "env/rerun_rrt",
-            "env/num_completed_paths", "env/behavior_state", "env/check_state",
+            "env/goal", "env/object_location", "env/rerun_rrt", "env/state_step",
+            "env/num_completed_paths", "env/behavior_state",
             "ik/rtol", "ik/atol", "ik/etol", "ik/p1", "ik/p2", "ik/p3",
             "robot/pose","robot/robot", "robot/parts", "robot/vL", "robot/vR", 
             "robot/velocity_rate", "robot/ts", "robot/reverse"
@@ -35,8 +35,8 @@ class Blackboard:
         # Setting variables
         # py_trees.logging.level = py_trees.logging.Level.DEBUG
         writer.debug = False
-        writer.controller_type = 'manual'
-        # writer.controller_type = 'autonomous'
+        # writer.controller_type = 'manual'
+        writer.controller_type = 'autonomous'
 
         writer.constants.robot = RobotConstants()
         writer.constants.lidar = LidarConstants()
@@ -51,12 +51,12 @@ class Blackboard:
         writer.env.map = Map()
         writer.env.behavior_state = 0
         writer.env.rerun_rrt = True
-        writer.env.check_state = True
         writer.env.num_completed_paths = 0
         # writer.env.map.map = np.load('C:\\Users\\joshk\\OneDrive\\Desktop\\CSCI 3302 - Intro to Robotics\\Hephaestus\\controllers\\grocery_shopper\\assets\\map.npy')
         writer.env.refresh_hz = 50
         writer.env.ftol = 1
         writer.env.state = 0
+        writer.env.state_step = 15
         writer.env.goal = None
         writer.env.waypoints = None
         # writer.env.waypoints = [
