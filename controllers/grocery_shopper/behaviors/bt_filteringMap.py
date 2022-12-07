8306d35f290101b5feaf824ca9c0f49efc8c2da4
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-12-06 22:59:03 -0700

    move convolving features to file

--- controllers/grocery_shopper/behaviors/bt_filteringMap.py
+++ controllers/grocery_shopper/behaviors/bt_filteringMap.py
@@ -1,6 +1,8 @@
 import py_trees
 import numpy as np
 from scipy.signal import convolve2d
+from .models import DisplayOverlays
+from .models import ConfigSpace
 
 class FilteringMap(py_trees.behaviour.Behaviour):
     def __init__(self, name, writer, reader):
@@ -13,18 +15,11 @@ class FilteringMap(py_trees.behaviour.Behaviour):
 
     def setup(self):
         self.log_message("setup()")
-        self.horizontal_mask = np.array([
-            [-1, -1, -1],
-            [ 2,  2,  2],
-            [-1, -1, -1],
-        ])
-        self.vertical_mask = np.array([
-            [-1, 2, -1],
-            [-1, 2, -1],
-            [-1, 2, -1],
-        ])
+        
         self.counter = 0
         self.frequency = self.r.env.refresh_hz
+        self.display = DisplayOverlays(self.w, self.r)
+        self.configSpace = ConfigSpace()
 
     def initialise(self):
         # self.log_message("initialise()")
@@ -35,9 +30,12 @@ class FilteringMap(py_trees.behaviour.Behaviour):
         self.feedback_message = f"Convolving Map in {self.frequency - self.counter}"
         if (self.counter%self.frequency == 0):
             self.feedback_message = "Convolving Map"
+            self.counter = 0
 
-            map = self.filter_by_convolving()
-            self.redraw_display(map)
+            map = self.configSpace.filter_by_convolving(self.r.env.map.map, self.r.env.ftol)
+            self.w.env.map.update_map(map)
+            self.display.redraw_display(map)
+            self.display.draw_waypoints()
             
         self.log_message("update()", self.feedback_message)
         return py_trees.common.Status.SUCCESS
@@ -45,36 +43,3 @@ class FilteringMap(py_trees.behaviour.Behaviour):
     def terminate(self, new_status):
         # self.log_message("terminate()", "%s->%s" % (self.status, new_status))
         pass
-
-    def filter_by_convolving(self):
-        map = self.r.env.map.map
-        grad = convolve2d(map, self.horizontal_mask, mode='same')
-        grad += convolve2d(map, self.vertical_mask, mode='same')
-        idx = grad >= self.r.env.ftol
-        grad[~idx] = 0
-        grad[idx] = map[idx]
-        grad[idx] = np.clip(grad[idx], 0.0, 1.0)
-        return grad
-
-    def redraw_display(self, map):
-            self.w.env.map.update_map(map)
-            width, height = map.shape
-            map = [(map.T*255).astype(int).tolist()]*3
-
-            # Reload new map
-            display = self.r.device.display
-            ir = display.imageNew(map, display.RGB, width, height)
-            display.imagePaste(ir, 0, 0, False)
-            display.imageDelete(ir)
-
-            # Draw waypoints on display as green path
-            display.setColor(0x00FF00)
-            for waypoint in self.r.env.waypoints:
-                wx, wy = self.get_display_coords(waypoint[0],waypoint[1])
-                display.drawPixel(wx, wy)
-
-    def get_display_coords(self, x, y, display=(360, 360), world=(30, 15)):
-        x = (display[0]*0.5) - (x * (display[0]/world[0]))
-        y = display[1] - ((display[1]*0.5) - (y * (display[1]/world[1])))
-        x, y = np.clip(x, 0, display[0]-1), np.clip(y, 0, display[1]-1)
-        return [int(x), int(y)]
