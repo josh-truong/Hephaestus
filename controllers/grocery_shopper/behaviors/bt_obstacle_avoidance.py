fa1b994345e742796c118eac27e78b74adb26a3d
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-12-06 18:25:02 -0700

    combine map check and lidar distance

--- controllers/grocery_shopper/behaviors/bt_obstacle_avoidance.py
+++ controllers/grocery_shopper/behaviors/bt_obstacle_avoidance.py
@@ -2,6 +2,8 @@ import py_trees
 import numpy as np
 from scipy.signal import convolve2d
 from .models import ConfigSpace
+from .models import DisplayOverlays
+import matplotlib.pyplot as plt
 
 class ObstacleAvoidance(py_trees.behaviour.Behaviour):
     def __init__(self, name, writer, reader):
@@ -14,62 +16,60 @@ class ObstacleAvoidance(py_trees.behaviour.Behaviour):
 
     def setup(self):
         self.log_message("setup()")
-        self.counter = 0
         self.frequency = 200
+        self.counter = 0
+        self.display = DisplayOverlays(self.w, self.r)
+        self.config_map = ConfigSpace().run(self.r.env.map.map)
 
     def initialise(self):
         self.log_message("initialise()")
+        self.counter = 0
 
     def update(self):
-        def get_display_coords(x, y, display=(360, 360), world=(30, 15)):
-            x = (display[0]*0.5) - (x * (display[0]/world[0]))
-            y = display[1] - ((display[1]*0.5) - (y * (display[1]/world[1])))
-            x, y = np.clip(x, 0, display[0]-1), np.clip(y, 0, display[1]-1)
-            return [int(x), int(y)]
+
         def get_lidar_readings():
             lidar = self.r.device.lidar
             lidar_sensor_readings = lidar.getRangeImage()
             lidar_sensor_readings = lidar_sensor_readings[83:len(lidar_sensor_readings)-83]
             return lidar_sensor_readings
+        # Get the 25th percentile of lidar readings
+        lidar_readings = np.array(get_lidar_readings())
+        lidar_idx = np.where(lidar_readings <= np.percentile(lidar_readings, 25))[0]
+        mean_depth = np.mean(lidar_readings[lidar_idx])
+        self.log_message("update()", f"Nearest object: {mean_depth:.3f} m.")
+
+        if (mean_depth < 1):
+            ### ADD STEERING FUNCTION HERE
+            left_readings = np.where(lidar_idx < len(lidar_readings)//2)[0]
+            right_readings = np.where(lidar_idx > len(lidar_readings)//2)[0]
+            
+            if (len(left_readings) > len(right_readings)):
+                self.log_message("update()", "Obstacle Detected on the left!")
+                print("Obstacle Detected on the left!")
+            else:
+                self.log_message("update()", "Obstacle Detected on the right!")
+                print("Obstacle Detected on the right!")
+
+        self.log_message("update()", f"Next state check in {self.frequency-self.counter}.")
+        self.counter += 1
+        if (self.counter%self.frequency == 0):
+            self.counter = 0
+            self.config_map = ConfigSpace().run(self.r.env.map.map)
+            plt.imshow(self.config_map)
+            plt.show()
+
+        waypoints = self.r.env.waypoints
+        for i in range(self.r.env.state_step*2):
+            i = np.clip(self.r.env.state-i, 0, len(waypoints)-1)
+            x, y = waypoints[i]
+            x, y = self.display.get_display_coords(x, y)
+            if (self.config_map[y][x] == 1):
+                print("Rerun rrt")
+            print(i, self.config_map[y][x])
+        
+        return py_trees.common.Status.SUCCESS
 
         
-        lidar_readings = np.array(get_lidar_readings())
-        idx = np.argsort(lidar_readings)[:50]
-        mean_dist = np.mean(lidar_readings[idx])
-
-        if (mean_dist < 0.4):
-            self.feedback_message = "Obstacle Detected!"
-            self.w.env.rerun_rrt = True
-            self.log_message("update()", self.feedback_message)
-            return py_trees.common.Status.SUCCESS
-
-        self.counter += 1
-        if (self.r.env.check_state):
-            self.w.env.check_state = False
-        # elif(self.counter == 100):
-        #     self.feedback_message = "Obstacle Detected!"
-        #     self.w.env.rerun_rrt = True
-        #     self.log_message("update()", self.feedback_message)
-        #     return py_trees.common.Status.SUCCESS
-        else:
-            return py_trees.common.Status.SUCCESS
-
-
-        self.counter = 0
-        waypoints = self.r.env.waypoints
-        state = self.r.env.state
-        state = state if (state == len(waypoints)-1) else state+1
-        waypoint = waypoints[state]
-
-        x, y = get_display_coords(waypoint[0], waypoint[1])
-        map = ConfigSpace().run(self.r.env.map.map)
-        self.feedback_message = ""
-        if (map[y][x] == 1):
-            self.w.robot.reverse = True
-            self.feedback_message = "Obstacle Detected!"
-        self.log_message("update()", self.feedback_message)
-        return py_trees.common.Status.SUCCESS
-        # return py_trees.common.Status.RUNNING
         
     def terminate(self, new_status):
         self.log_message("terminate()", "%s->%s" % (self.status, new_status))
