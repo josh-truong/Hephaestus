32362680d572c5d150da7a143e4d7fa368b19621
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-12-06 18:22:41 -0700

    removed theta from rrt return

--- controllers/grocery_shopper/behaviors/models/planning.py
+++ controllers/grocery_shopper/behaviors/models/planning.py
@@ -177,7 +177,7 @@ class Planning:
     def get_world_coords(self, x, y, display=(360, 360), world=(30, 15)):
         x = ((display[0]*0.5) - x) / (display[0]/world[0])
         y = (y + (display[1]*0.5) - display[1]) / (display[1]/world[1])
-        return x,-y,0
+        return x,-y
 
     def getWaypoints(self, nodes):
         # list of waypoints in map coords, tulpes with (x, y, theta)
