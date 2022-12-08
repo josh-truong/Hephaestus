d034f5e033ebdf7632f766de23f006fffc93c5e0
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-12-08 15:46:32 -0700

    Update bt_rrt.py

--- controllers/grocery_shopper/behaviors/bt_rrt.py
+++ controllers/grocery_shopper/behaviors/bt_rrt.py
@@ -30,12 +30,9 @@ class RRT(py_trees.behaviour.Behaviour):
 
         pose = self.r.robot.pose
         x, y = self.display.get_display_coords(pose.x, -pose.y)
-        map = ConfigSpace().run(self.r.env.map.map)
-
+        # map = ConfigSpace().run(self.r.env.map.map)
+        map = self.r.env.map.map
         from_point = np.array([x, y])
-        # xbound = self.w.env.xmax_boundary
-        # ybound = self.w.env.ymax_boundary
-        # to_point = np.array([np.random.randint(0,xbound), np.random.randint(0,ybound)])
         to_point = np.random.randint(0,360, 2)
         nodes = self.planner.rrt([x, y], to_point, 1000, 10, map)
         if (not nodes):
