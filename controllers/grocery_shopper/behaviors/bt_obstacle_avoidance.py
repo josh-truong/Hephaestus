97b7450354a19ac7d7336522a5da552376de8782
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-12-08 04:07:06 -0700

    Obstacle avoidance with waypoint checking and distance

--- controllers/grocery_shopper/behaviors/bt_obstacle_avoidance.py
+++ controllers/grocery_shopper/behaviors/bt_obstacle_avoidance.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from .models import ControllerModel
 from .models import Localization
 
-from .bt_reverse import Reverse
 from .bt_rrt import RRT
 
 class ObstacleAvoidance(py_trees.behaviour.Behaviour):
@@ -15,10 +14,10 @@ class ObstacleAvoidance(py_trees.behaviour.Behaviour):
         super(ObstacleAvoidance, self).__init__(name)
         # self.logger.debug("%s [%s::__init__()]" % (self.name, self.__class__.__name__))
         self.w, self.r = writer, reader
-        self.reverse_behavior = py_trees.composites.Sequence("Reverse Sequence")
-        self.reverse_behavior.add_child(Reverse("Reversing", self.w, self.r))
-        self.reverse_behavior.add_child(RRT("RRT for reverse", self.w, self.r))
-        self.reverse_behavior.setup_with_descendants()
+        # self.reverse_behavior = py_trees.composites.Sequence("Reverse Sequence")
+        # self.reverse_behavior.add_child(Reverse("Reversing", self.w, self.r))
+        # self.reverse_behavior.add_child(RRT("RRT for reverse", self.w, self.r))
+        # self.reverse_behavior.setup_with_descendants()
 
     def log_message(self, function_name: str, feedback_message=""):
         self.logger.debug("%s [%s::%s][%s]" % (self.name, function_name, self.__class__.__name__, feedback_message))
@@ -28,94 +27,62 @@ class ObstacleAvoidance(py_trees.behaviour.Behaviour):
         self.frequency = 200
         self.counter = 0
         self.display = DisplayOverlays(self.w, self.r)
-        self.config_map = ConfigSpace().run(self.r.env.map.map)
         self.Driver = ControllerModel(self.w, self.r)
-        self.localization = Localization(self.w, self.r)
-        self.linspace_waypoints = None
-        self.prev_endstate = None
-        self.prev_pose = self.r.robot.pose
-        self.n_stuck = 0
-        self.max_stuck = 500
 
-        
+        # Tracking New Behavior if triggered
+        self.detect_depth  = 0.7
+        self.desired_depth = 1.5
+        self.current_depth = self.detect_depth
+        self.switch = False
+
 
     def initialise(self):
         # self.log_message("initialise()")
         pass
 
     def update(self):
+        ######################################
+        # Continously check a subset of waypoints to determine if path is still valid
+        waypoints = self.r.env.waypoints
+        n = len(waypoints)
+        state, state_step = self.r.env.state, self.r.env.state_step
+        endstate = np.clip(state+state_step*3, 0, n-1)
 
-        # pose =  self.r.robot.pose
-        # if (self.n_stuck%self.max_stuck==0 and self.n_stuck!=0):
-        #     self.w.env.rerun_rrt = True
-        #     self.n_stuck = 0
-        #     print("Max stuck")
-        # elif (abs(pose.x-self.prev_pose.x)+abs(pose.y-self.prev_pose.y) < 1):
-        #     self.n_stuck += 1
-        #     print(f"Detected Stuck {self.n_stuck} {abs(pose.x-self.prev_pose.x)+abs(pose.y-self.prev_pose.y)}")
-        # elif (self.counter%50 == 0):
-        #     self.prev_pose =  self.r.robot.pose
-        #     print("update prev pose")
-        # else:
-        #     self.n_stuck = 0
-        #     print(f"not stuck {abs(pose.x-self.prev_pose.x)+abs(pose.y-self.prev_pose.y)}")
+        waypoints = [self.display.get_display_coords(pt[0], pt[1]) for pt in waypoints[state:endstate]]
+        waypoints = np.array(waypoints)
+        map = np.array(self.r.env.map.map)[waypoints]
+        if (len(np.where(map == 1)[0])):
+            self.w.env.rerun_rrt = self.switch
+            self.w.robot.msg = "Path conflict."
 
-        # self.log_message("update()", f"Next state check in {self.frequency-self.counter}.")
-        # self.counter += 1
-        # # Re-convolve map based on frequency since it is a performance problem
-        # if (self.counter%self.frequency == 0):
-        #     self.counter = 0
-        #     self.config_map = ConfigSpace().run(self.r.env.map.map)
+        ######################################
+        # Continously check the distance of the robot to avoid obstacle collision
+        def get_lidar_readings():
+            lidar = self.r.device.lidar
+            lidar_sensor_readings = lidar.getRangeImage()
+            lidar_sensor_readings = lidar_sensor_readings[83:len(lidar_sensor_readings)-83]
+            return lidar_sensor_readings
+        # Get the 25th percentile of lidar readings
+        lidar_readings = np.array(get_lidar_readings())
+        lidar_idx = np.where(lidar_readings <= np.percentile(lidar_readings, 25))[0]
+        mean_depth = np.mean(lidar_readings[lidar_idx])
 
-        # # # Recheck if path is still valid
-        # waypoints = self.r.env.waypoints
-        # n = len(waypoints)
-        # state, state_step = self.r.env.state, self.r.env.state_step
-        # endstate = np.clip(state+state_step*3, 0, n-1)
-        # if (self.prev_endstate != endstate):
-        #     self.prev_endstate = endstate
-
-        #     startstate = np.clip(state-state_step, 0, n-1)
-        #     startpoint, endpoint = waypoints[startstate], waypoints[endstate]
-        #     manhatten_distance = int(abs(startpoint[0]-endpoint[0]) + abs(startpoint[1]-endpoint[1]))
-        #     self.linspace_waypoints = np.linspace(startpoint, endpoint, manhatten_distance)
-
-        # for x, y in self.linspace_waypoints:
-        #     x, y = self.display.get_display_coords(x, y)
-        #     if (self.config_map[y][x] == 1):
-        #         self.log_message("update()", "Invalid path found! Rerunning RRT.")
-        #         self.w.env.rerun_rrt = True
-        #         self.w.env.state = np.clip(self.r.env.state-2, 0, n-1)
-        #         break
-
-        self.reverse_behavior.tick_once()
-
-
-
-
-
-
-        # Steer robot if obstacle is detected
-        # if (mean_depth < 0.9):
-            # left_readings = np.where(lidar_idx < len(lidar_readings)//2)[0]
-            # right_readings = np.where(lidar_idx > len(lidar_readings)//2)[0]
-            # velocity_rate = self.r.robot.velocity_rate
-            # speed = self.r.constants.robot.MAX_SPEED
-            # if (len(left_readings) > len(right_readings)):
-            #     self.w.robot.vL, self.w.robot.vR = 0.2*speed, -0.2*speed
-            # else:
-            #     self.w.robot.vL, self.w.robot.vR = -0.2*speed, 0.2*speed
-
-            # self.Driver.set_wheel_joint_vel(self.r.robot.vL, self.r.robot.vR)
-            # self.Driver.localization.update_odometry()
-
-            # # Find the farthest waypoint given a radius
-            # waypoints = np.array(self.r.env.waypoints)
-            # pose =  self.r.robot.pose
-            # pose = np.array([pose.x, pose.y])
-            # state = np.argmin(np.linalg.norm(waypoints-pose, axis=1))
-            # self.w.env.state = np.clip(state+1, 0, len(waypoints)-1)
-            # return py_trees.common.Status.RUNNING
+        if (mean_depth < self.current_depth):
+            self.log_message("update()", f"Nearest object: {mean_depth:.3f} m.")
+            self.w.robot.msg = "Going backwards"
+            self.current_depth = self.desired_depth
+            self.switch = True
+            
+            speed = self.r.constants.robot.MAX_SPEED
+            self.Driver.set_wheel_joint_vel(-speed, -speed)
+            self.Driver.localization.update_odometry()
+            return py_trees.common.Status.RUNNING
+        elif (self.switch):
+            self.w.robot.msg = "Calling RRT to run."
+            self.w.env.rerun_rrt = self.switch
+            self.switch = False
+        else:
+            self.current_depth = self.detect_depth
         return py_trees.common.Status.SUCCESS
 
     def terminate(self, new_status):
