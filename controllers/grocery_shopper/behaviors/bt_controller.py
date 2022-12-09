5d8012032d5e5aaadbc32068c76170894391620e
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-12-09 12:57:56 -0700

    Update bt_controller.py

--- controllers/grocery_shopper/behaviors/bt_controller.py
+++ controllers/grocery_shopper/behaviors/bt_controller.py
@@ -1,10 +1,10 @@
 import py_trees
 import math
 import numpy as np
-from .models import Pose, ControllerModel
+from .models import Pose, SpeedController
 from .models import Planning
-from scipy.signal import convolve2d
 from .models import Localization
+from .models import ConfigSpace
 
 class Controller(py_trees.behaviour.Behaviour):
     def __init__(self, name, writer, reader):
@@ -18,8 +18,7 @@ class Controller(py_trees.behaviour.Behaviour):
 
     def setup(self):
         # self.log_message("setup()")
-        self.Driver = ControllerModel(self.w, self.r)
-
+        self.Driver = SpeedController(self.w, self.r)
 
     def initialise(self):
         # self.log_message("initialise()")
@@ -32,41 +31,15 @@ class Controller(py_trees.behaviour.Behaviour):
         if (key == ord('R')):
             self.w.env.rerun_rrt = True
         elif (key == ord('F')):
-            def expand(img_mask, cur_coord):
-                coordinates_in_blob = []
-                coordinate_list = [cur_coord]
-                while len(coordinate_list) > 0:
-                    (y, x) = coordinate_list.pop()
-                    if y < 0 or x < 0 or y >= img_mask.shape[0] or x >= img_mask.shape[1]:
-                        continue
-                    if img_mask[y, x] == 0.0:
-                        continue
-                    img_mask[y,x] = 0
-                    coordinates_in_blob.append([x,y])
-                    coordinate_list.extend([(y-1, x),(y+1, x),(y, x-1),(y, x+1)])
-                return np.asarray(coordinates_in_blob)
-
-            def get_blobs(img_mask):
-                img_mask_height, img_mask_width = img_mask.shape[0], img_mask.shape[1]
-                blobs_list, blobs_size = [], []
-                img_mask_copy = img_mask.copy()
-                for idx in range(img_mask_height*img_mask_width):
-                    y, x = idx // img_mask_width, idx % img_mask_width
-                    if (img_mask[y][x] > 0):
-                        blob_coords = expand(img_mask_copy, [y, x])
-                        if (len(blob_coords)):
-                            blobs_list.append(blob_coords)
-                            blobs_size.append(len(blob_coords))
-                return np.array(blobs_list), np.array(blobs_size)
-            
-            blobs_list, blobs_size = get_blobs(self.r.env.map.map.copy())
-            outlier_idx = np.where(blobs_size < 30)
-            outliers = blobs_list[outlier_idx]
-            if (outliers != []):
-                for x, y in np.vstack(outliers):
-                    self.w.env.map.update_pixel(x,y,0)
+            map = ConfigSpace().filter_small_islands(self.r.env.map.map)
+            self.w.env.map.update_map(map)
         elif (key == ord('Q')):
-            self.w.env.behavior_state += 1
+            self.w.env.behavior_state = 1
+        elif (key == ord('S')):
+            np.save("assets/map.npy", self.r.env.map.map)
+            np.save("assets/viable_waypoints.npy", self.r.env.vaiable_waypoints)
+            np.save("assets/object_location.npy", self.r.env.object_location)
+            print("Map, waypoints, est. cube location saved!")
 
         vL, vR = 0, 0
         controller_type = self.r.controller_type
