b6331feaab306921717cf1e725e2751fb9fbe9a0
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-12-09 13:38:15 -0700

    Update __init__.py

--- controllers/grocery_shopper/behaviors/__init__.py
+++ controllers/grocery_shopper/behaviors/__init__.py
@@ -1,12 +1,12 @@
 from .blackboard import Blackboard
+from .bt_camera_vision import CameraVision
 from .bt_controller import Controller
-from .bt_mapping import Mapping
 from .bt_denoise_map import DenoiseMap
+from .bt_kmeans import Kmeans
 from .bt_line_detection import LineDetection
 from .bt_map_boundings import MapBounds
-from .bt_camera_vision import CameraVision
+from .bt_mapping import Mapping
 from .bt_obstacle_avoidance import ObstacleAvoidance
-from .bt_rrt import RRT
-from .bt_reverse import Reverse
-
 from .models import Planning
+from .bt_reverse import Reverse
+from .bt_rrt import RRT
