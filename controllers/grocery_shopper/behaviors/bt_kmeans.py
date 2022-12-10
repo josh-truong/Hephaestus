4324761295d7f2126f39ba42740cf9c06f0ca8a5
Joshua Truong <86546543+josh-truong@users.noreply.github.com>
2022-12-09 18:48:19 -0700

    Update bt_kmeans.py

--- controllers/grocery_shopper/behaviors/bt_kmeans.py
+++ controllers/grocery_shopper/behaviors/bt_kmeans.py
@@ -40,8 +40,34 @@ class KMeans(py_trees.behaviour.Behaviour):
         clusters = kmeans.run(raw_x, raw_y, n_cluster)
         centroids = np.vstack([clusters.center_x, clusters.center_y, np.zeros(clusters.n_label)]).T
         self.w.env.kmeans_location = centroids.tolist()
+        self.editWaypoints()
         self.display.draw_estimated_location_on_map(self.r.env.kmeans_location, color=0x00FF00)
         return py_trees.common.Status.SUCCESS
         
     def terminate(self, new_status):
         self.log_message("terminate()", "%s->%s" % (self.status, new_status))
+
+
+
+    def editWaypoints(self):
+        map = self.r.env.map.map
+        for i in range(len(self.r.env.kmeans_location)):
+            wX,wY,depth = self.r.env.kmeans_location[i]
+            x, y = self.display.get_display_coords(wX,wY)
+            upOffset, downOffset = 0, 0
+            while y + upOffset <= 360 and map[y + upOffset][x] == 1:
+                upOffset += 1
+            while y - downOffset >= 0 and map[y - downOffset,x] == 1:
+                downOffset += 1
+
+            upWorld = self.display.get_world_coords(x,y + upOffset)
+            downWorld = self.display.get_world_coords(x,y - upOffset)
+
+            if (y - downOffset == -1):
+                self.w.env.kmeans_location[i] = [wX, upWorld[1], depth]
+            elif (y + upOffset == 361):
+                self.w.env.kmeans_location[i] = [wX, downWorld[1], depth]
+            elif (upOffset < downOffset):
+                self.w.env.kmeans_location[i] = [wX, upWorld[1], depth]
+            else:
+                self.w.env.kmeans_location[i] = [wX, downWorld[1], depth]
